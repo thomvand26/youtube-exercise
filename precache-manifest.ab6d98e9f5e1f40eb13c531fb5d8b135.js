36c5a20f61f0773e3e223370fa6ae5ebd4f928f2
thomvand26 <thomvand26@student.arteveldehs.be>
2020-03-01 11:05:33 +0100

    update build

---
+++ precache-manifest.ab6d98e9f5e1f40eb13c531fb5d8b135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439a802a686b6e53b0e5087cff3da11b",
+    "url": "/youtube-exercise/index.html"
+  },
+  {
+    "revision": "52483ecd38150666cf8f",
+    "url": "/youtube-exercise/static/css/main.fabdba04.chunk.css"
+  },
+  {
+    "revision": "3dc00f4eb6c5a45eb0ac",
+    "url": "/youtube-exercise/static/js/2.7c476d3c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/youtube-exercise/static/js/2.7c476d3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52483ecd38150666cf8f",
+    "url": "/youtube-exercise/static/js/main.cf49b4da.chunk.js"
+  },
+  {
+    "revision": "d4b7f9fa6075856efb76",
+    "url": "/youtube-exercise/static/js/runtime-main.5b0ba59e.js"
+  }
+]);
